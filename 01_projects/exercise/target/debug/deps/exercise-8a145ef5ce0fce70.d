427b6f002e906cc63e5c38eb9e87c8c47df37957
brett-me <its-me-brett@protonmail.com>
2025-03-27 20:15:17 +0200

    add: new exercise from comprehensive rust

---
+++ 01_projects/exercise/target/debug/deps/exercise-8a145ef5ce0fce70.d
@@ -0,0 +1,5 @@
+/home/brett/rust/01_projects/exercise/target/debug/deps/exercise-8a145ef5ce0fce70: src/main.rs
+
+/home/brett/rust/01_projects/exercise/target/debug/deps/exercise-8a145ef5ce0fce70.d: src/main.rs
+
+src/main.rs:
---
+++ 01_projects/exercise/target/debug/exercise.d
@@ -0,0 +1,1 @@
+/home/brett/rust/01_projects/exercise/target/debug/exercise: /home/brett/rust/01_projects/exercise/src/main.rs
