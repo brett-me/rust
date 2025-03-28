05f3ad9a0188ebead14b72630bf32e6f56342c8d
brett-me <its-me-brett@protonmail.com>
2025-03-28 20:50:33 +0200

    edit: point 3 changed 'build' to 'run'

--- 01_projects/revision/src/main.rs
+++ 01_projects/revision/src/main.rs
@@ -2,5 +2,5 @@ fn main() {
     println!("Cargo revision");
     println!("1. Cargo new (filename)");
     println!("2. CD into directory");
-    println!("3. Cargo build");
+    println!("3. Cargo run");
 }
