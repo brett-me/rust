e236d3ab99f4c56a4ade0888172226bcf12c534e
brett-me <its-me-brett@protonmail.com>
2025-03-29 21:58:49 +0200

    init: guessing_game project

---
+++ 01_projects/guessing_game/target/debug/deps/guessing_game-bcaa7737c56b17cb.d
@@ -0,0 +1,5 @@
+/home/brett/rust/01_projects/guessing_game/target/debug/deps/guessing_game-bcaa7737c56b17cb: src/main.rs
+
+/home/brett/rust/01_projects/guessing_game/target/debug/deps/guessing_game-bcaa7737c56b17cb.d: src/main.rs
+
+src/main.rs:
---
+++ 01_projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/brett/rust/01_projects/guessing_game/target/debug/guessing_game: /home/brett/rust/01_projects/guessing_game/src/main.rs
